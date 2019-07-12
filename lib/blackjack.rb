6aead5dfba123f155997ad2a96d848c8a667761c
Ross Perkel <rossperkel@gmail.com>
2019-07-12 16:19:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,6 +50,13 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  hit?
+  display_card_total
+  if card_total > 21
+  end_game
+end
+
 end
     
